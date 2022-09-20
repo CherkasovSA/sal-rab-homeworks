1163be5224b307cbedd57f518f8e3d335493bc3a
Sergey Cherkasov <pamiec95@gmail.com>
2022-09-20 09:49:26 +0400

    task 1.3 v2

--- task_1/solution.js
+++ task_1/solution.js
@@ -39,9 +39,8 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
-    let newSum = oldSum + difference
-    let newSumText = newSum + ' ₽'
-	newSumText = ${newSum} ₽
+    
+	
 
     // Конец решения задания №1.3.
 
