ed9af5e37df63be3ce3302fe4230dba542e865d2
Sergey Cherkasov <pamiec95@gmail.com>
2022-09-20 08:19:56 +0400

    task_1.1 v2

--- task_1/solution.js
+++ task_1/solution.js
@@ -5,13 +5,13 @@ function addInCartMessage(title, price) {
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // let message
-    // message = productName
-    // message = productName 
-    // message = productName 'за'
-    // message = productName 'за' 
-    // message = productName 'за' productPrice
-    // message = productName 'за' productPrice 
-    // message = productName 'за' productPrice 'теперь в корзине!'
+    // let message = productName
+    // let message = productName 
+    // let message = productName 'за'
+    // let message = productName 'за' 
+    // let message = productName 'за' productPrice
+    // let message = productName 'за' productPrice 
+    // let message = productName 'за' productPrice 'теперь в корзине!'
     // console.log(message)
 
     // Конец решения задания №1.1.
